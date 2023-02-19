8544e8527804cee05f5459a7bca62b5bb67bc451
AlexandrSawrasov <caiiiuk@mail.ru>
2023-02-19 20:44:30 +0300

    add getRandomArrListFrom0To100

--- task1.java
+++ task1.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class task1 {
 
@@ -5,6 +7,16 @@ public class task1 {
         // Создать список рандомных чисел от 0 до 100 (20 чисел)
         // Отсортировать этот список
         // Вывести на экран
-        
+
     }
+
+    public static ArrayList<Integer> getRandomArrListFrom0To100(int len) {
+        Random rand = new Random();
+        ArrayList<Integer> randArr = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            randArr.add(rand.nextInt(100));
+        }
+        return randArr;
+    }
+
 }
